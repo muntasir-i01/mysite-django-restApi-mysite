77d7f118016f03193601d10ec13478541662b2c7
muntasir-i01 <muntasirm55@gmail.com>
2024-02-22 11:39:04 +0600

    add modelform from form class in serializers.py

--- mysite/api_basic/serializers.py
+++ mysite/api_basic/serializers.py
@@ -2,21 +2,8 @@ from rest_framework import serializers
 from .models import Article
 
 
-class ArticleSerializers(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    author = serializers.CharField(max_length=100)
-    email = serializers.EmailField(max_length=100)
-    date = serializers.DateField()
-
-
-    def create(self, validated_data):
-        return super().create(validated_data)
+class ArticleSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['title', 'author', 'email', 'date']
     
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.author = validated_data.get('author', instance.author)
-        instance.email = validated_data.get('email', instance.email)
-        instance.date = validated_data.get('date', instance.date)
-        instance.save()
-        return instance
