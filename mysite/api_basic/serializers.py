304fbe160d7a17cd3ea3d9214f2d75068419b73a
muntasir-i01 <muntasirm55@gmail.com>
2024-02-25 16:02:00 +0600

    now use api decorator & function based api view which provides a set of sample decorators that wrap our function based views to ensure their receive & stands as an instance of request rather than a usual jango http and all and also it allows them to return a response instead of django HTTP response, also the benefit of decorator api view is that you can access to django where browsable api view

--- mysite/api_basic/serializers.py
+++ mysite/api_basic/serializers.py
@@ -5,7 +5,7 @@ from .models import Article
 class ArticleSerializers(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['title', 'author', 'email', 'date']
+        fields = ['id','title', 'author', 'email', 'date']
 
 
 
--- mysite/api_basic/views.py
+++ mysite/api_basic/views.py
@@ -4,28 +4,31 @@ from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializers
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+# api_view decorator in function based api view 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def article_list(request):
 
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializers(articles, many = True)
-        return JsonResponse(serializer.data, safe = False)
+        return Response(serializer.data)
 
     
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializers(data=data)
+        serializer = ArticleSerializers(data=request.data)
 
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return JsonResponse(serializer.errors, status=400)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_CREATD)
 
 
 
