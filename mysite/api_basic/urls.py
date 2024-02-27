f39ea600984dc793c1e552282b60ceb0dd83be99
muntasir-i01 <muntasirm55@gmail.com>
2024-02-27 15:52:50 +0600

    crud operations worked for generic(needed look_up field = id api view and also updated the url/api_basic.py

--- mysite/api_basic/urls.py
+++ mysite/api_basic/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import article_list, article_detail, ArticleAPIView, ArticleDetails
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetails, GenericAPIView
 
 
 urlpatterns = [
     # path('article/', article_list),
-    path('article/', ArticleAPIView.as_view()),
+    #path('article/', ArticleAPIView.as_view()),
+    path('article/<int:id>/', GenericAPIView.as_view()),
     # path('details/<int:pk>/', article_detail),
     path('details/<int:id>/', ArticleDetails.as_view()), 
 ]   
--- mysite/api_basic/views.py
+++ mysite/api_basic/views.py
@@ -13,6 +13,32 @@ from rest_framework import mixins
 
 # Create your views here.
 
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializers
+    queryset = Article.objects.all()
+
+    lookup_field = 'id'
+
+    def get(self, request, id=None):
+
+        if id:
+            return self.retrieve(request)
+
+        else:
+            return self.list(request)
+
+
+    def post(self, request):
+        return self.create(request)
+
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
+
 # class based API view
 class ArticleAPIView(APIView):
 
