4267ddf0c6349152872c65d3de6d1ee885adf716
muntasir-i01 <muntasirm55@gmail.com>
2024-02-27 14:28:08 +0600

    crud operations worked for class based api view and also updated the url/api_basic.py

--- mysite/api_basic/urls.py
+++ mysite/api_basic/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetails
 
 
 urlpatterns = [
-    path('article/', article_list),
-    path('details/<int:pk>/', article_detail),
+    # path('article/', article_list),
+    path('article/', ArticleAPIView.as_view()),
+    # path('details/<int:pk>/', article_detail),
+    path('details/<int:id>/', ArticleDetails.as_view()), 
 ]   
