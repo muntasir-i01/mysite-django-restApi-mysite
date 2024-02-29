5ad5e4f1302bbbc951314d9768a6cccc97b6b86f
muntasir-i01 <muntasirm55@gmail.com>
2024-02-29 12:19:57 +0600

    import session & basic authentication, IsAuthenticated & add authentication set the authentication scheme on a per-view or per-view

--- mysite/api_basic/views.py
+++ mysite/api_basic/views.py
@@ -10,6 +10,8 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -18,6 +20,8 @@ class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.Crea
     queryset = Article.objects.all()
 
     lookup_field = 'id'
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, id=None):
 
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api_basic',
     'mysite',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
